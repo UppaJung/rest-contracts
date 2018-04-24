3f1ec2f1c9f46754fe2a5b75cf074e8f7931ab2e
Stuart Schechter <stuart@revisolution.com>
2018-04-24 17:22:20 +0900

    remove release 0.9 to help deployment script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,5 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
-<a name="0.9.0"></a>
-# 0.9.0 (2018-04-24)
-Test release awaiting partner modules.
-
 
 # Change Log
---
+++ src/sample.ts
@@ -0,0 +1,41 @@
+import * as RestContracts from "./rest-contracts";
+
+export interface Excuse {
+  id: number;
+  quality: 'Solid' | 'Iffy' | 'Lousy';
+  description: string;
+}
+
+
+// export const Get =
+//   RestContracts.CreateAPI.Get
+//   .PathParameters<{ id: number }>()
+//   .NoQueryParameters
+//   .Returns<Excuse>()
+//   .
+
+
+
+
+
+
+// export const Get =
+//   RestContracts.CreateAPI.Get
+//   .PathParameters<{ id: number }>()
+//   .NoQueryParameters
+//   .Returns<Excuse>()
+//   .Path('/excuses/:id/');
+
+export const Query =
+  RestContracts.CreateAPI.Get
+  .NoPathParameters
+  .QueryParameters<{quality?: Excuse["quality"]}>()
+  .Returns<Excuse>()
+  .Path('/excuses/');
+
+export const Put =
+  RestContracts.CreateAPI.Put
+  .NoPathParameters
+  .BodyParameters<Excuse>()
+  .Returns<Excuse>()
+  .Path("/excuses/");
