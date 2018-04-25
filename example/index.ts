9e1acd9a3fa8d82153888e9f368eacbba6395351
Stuart Schechter <stuart@revisolution.com>
2018-04-25 14:54:00 +0900

    add example investigate publish issues

---
+++ example/index.ts
@@ -0,0 +1,4 @@
+import * as RestContracts from "rest-contracts";
+// import * as RestContractsBrowserClient from "rest-contracts-browser-client";
+import * as RestContractsExpressServer from "rest-contracts-express-server";
+
