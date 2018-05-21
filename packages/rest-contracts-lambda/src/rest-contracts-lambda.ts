2e74084f779fe4e3148a9dd28405da55060470d4
Stuart Schechter <stuart@revisolution.com>
2018-05-21 12:26:09 +0900

    support multiple resonse encoding types add CORS support

--- packages/rest-contracts-lambda/src/rest-contracts-lambda.ts
+++ packages/rest-contracts-lambda/src/rest-contracts-lambda.ts
@@ -16,6 +16,17 @@ export interface AugmentedAPIGatewayProxyEvent<API extends RestContracts.API> ex
     RestContracts.BODY_PARAMS<API> | undefined
 }
 
+export enum ResultEncoding {
+  JSON = "JSON",
+  BinaryToBeBase64Encoded = "BinaryToBeBase64Encoded",
+  RawString = "RawString"
+}
+
+export type LambdaErrorHandler = (
+  err: any,
+  callback: Lambda.APIGatewayProxyCallback
+) => void;
+
 /**
  * Wrap a handler for a lambda API to automatically parse the body parameters,
  * create a simple way to access all parameters, and restrict parameter types
@@ -124,7 +135,9 @@ export const wrapLambdaParameters = <API extends RestContracts.API>(
  *       (This ordering was chosen because attackers are more likely to have control
  *        of the body contents than the query, and more likely to have control over
  *        the query parameters than the path parameters.)
- * @param errorHandler An optional error handler that catches exceptions and
+ * The final parameter contains the proxy result to be sent back to lambda,
+ * so that the handler can set a custom statusCode, body, or headers.
+ * @param options.errorHandler An optional error handler that catches exceptions and
  * handles them by calling the standard lambda callback function.  If not set,
  * a default exception handler looks for the HTTP status to return in either the
  * statusCode or status attribute of the error that was thrown, and sets the
@@ -133,37 +146,101 @@ export const wrapLambdaParameters = <API extends RestContracts.API>(
  * error as the first parameter and lets lambda figure out how to handle it
  * based on your configured integration reponse, as documented in:
  * https://docs.aws.amazon.com/apigateway/latest/developerguide/handle-errors-in-lambda-integration.html
+ * @param options.resultEncoding Defaults to JSON encoding.  If set to
+ * ResultEncoding.RawString then the result will not be JSON encoded if it's already a string.
+ * If set to ResultEncoding and the result is a buffer, it will be base64 encoded.
+ * @param options.defaultHeaders Sets response headres to be used for every call to this API
+ * @param options.corsDomains Sets the Access-Control-Allow-Origin resonse header for CORS support.
+ * Use "*" for all domains or a comma-separated list of domains
+ *
+ *
  */
 export const wrapLambdaParametersAndResult = <API extends RestContracts.API>(
   api: API,
   handler: (
     event: AugmentedAPIGatewayProxyEvent<API>,
-    context: Lambda.Context
+    context: Lambda.Context,
+    apiGatewayProxyResult: Partial<Lambda.APIGatewayProxyResult> & {headers: {[headerName: string]: string}}
   ) => PromiseLike<RestContracts.RESULT<API>> | RestContracts.RESULT<API>,
-  errorHandler: (
-    err: any,
-    callback: Lambda.APIGatewayProxyCallback
-  ) => void = defaultErrorHandler
-) => wrapLambdaParameters( api, async (
-  event: AugmentedAPIGatewayProxyEvent<API>,
-  context: Lambda.Context,
-  callback: Lambda.APIGatewayProxyCallback
+  options?: {
+    errorHandler?: LambdaErrorHandler,
+    defaultHeaders?: {[headerName: string]: string},
+    corsDomains?: "*" | string[],
+    resultEncoding?: ResultEncoding
+  } & (
+    {} |
+    {returnResultAsRawString: boolean} |
+    {base64EncodeBinaryResult: boolean}
+  )
 ) => {
-  try {
-    const result = await handler( event, context );
-    callback(undefined, {
-      // tslint:disable:no-magic-numbers
-      statusCode:
-        (api.method === RestContracts.Method.get && typeof(result) === "undefined") ? 404 :
-        (api.method === RestContracts.Method.put) ? 201 :
-        200,
-      // tslint:enable:no-magic-numbers
-      body: JSON.stringify(result)
-    });
-  } catch (err) {
-    errorHandler(err, callback);
+  const{
+    errorHandler = defaultErrorHandler,
+    defaultHeaders = {},
+    resultEncoding = "JSON",
+    // tslint:disable-next-line:no-unnecessary-initializer
+    corsDomains = undefined
+  } = options || {};
+  let corsDomainSet: Set<string>;
+  if (corsDomains === "*") {
+    // For CORS always-allowed, we can set a default header
+    defaultHeaders["Access-Control-Allow-Origin"] = "*";
+  } else if (Array.isArray(corsDomains)) {
+    // For CORS allowing only a specific list (set) of origins,
+    // we can create a set of origins (forcing to lowercase to
+    // make them case insensitive)
+    corsDomainSet = new Set(corsDomains.map( o => o.toLowerCase()));
   }
-});
+
+  return wrapLambdaParameters( api, async (
+    event: AugmentedAPIGatewayProxyEvent<API>,
+    context: Lambda.Context,
+    callback: Lambda.APIGatewayProxyCallback
+  ) => {
+    try {
+      const headers = {...defaultHeaders};
+      // If the origin is among the CORS-allowed origins specified in the corsDomains parameter,
+      // return the origin as the CORS-allowed origin to the client to let it know CORS is indeed allowed
+      if (corsDomainSet && corsDomainSet.has(event.headers.origin.toLowerCase()) ) {
+        // The origin is among those that are allowed to make cross-origin requests to this API
+        headers["Access-Control-Allow-Origin"] = event.headers.origin;
+      }
+      const apiResult: Partial<Lambda.APIGatewayProxyResult> & {headers: {[headerName: string]: string}} = {
+        headers
+      };
+      const handlersResult = await handler( event, context, apiResult );
+      // Create the correct statusCode for result provided by the handler
+      // tslint:disable:no-magic-numbers
+      const statusCode =
+          (api.method === RestContracts.Method.get && typeof(handlersResult) === "undefined") ? 404 :
+          (api.method === RestContracts.Method.put) ? 201 :
+          200;
+        // tslint:enable:no-magic-numbers
+
+      // Encode the body object as specified by the ResultEncoding option (JSON is default)
+      const bodyObject = (
+          resultEncoding === ResultEncoding.RawString &&
+            typeof(handlersResult) === "string"
+        ) ? {
+          body: handlersResult as string
+        } : (
+          resultEncoding === ResultEncoding.BinaryToBeBase64Encoded &&
+          typeof(handlersResult) === "object" &&
+          ((handlersResult as object) instanceof Buffer)
+        ) ? {
+          body: handlersResult.toString('base64'),
+          isBase64Encoded: true
+        } : {
+          body: JSON.stringify(handlersResult)
+        };
+        // Copy in any part of the API result set by the handler,
+        // which overrides any values we have set.
+
+      callback(undefined, {statusCode, ...bodyObject, ...apiResult});
+    } catch (err) {
+      errorHandler(err, callback);
+    }
+  });
+}
 
 const defaultErrorHandler = (err: any, callback: Lambda.APIGatewayProxyCallback) => {
   // Handle errors that provide a status code (via either status or statusCode field)
